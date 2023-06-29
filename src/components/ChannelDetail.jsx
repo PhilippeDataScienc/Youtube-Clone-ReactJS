8d32715cd8dae4cde2a5e670b1e8272260c4c1cf
Philippe <philippe.acquier@gmail.com>
2023-06-29 15:08:40 +0200

    modif up to VideoDetail.jsx

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -17,8 +17,23 @@ const ChannelDetail = () => {
     fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`)
     .then((data) => setVideos(data?.items));
   }, [id])
+
   return (
-    <Box>ChannelDetail</Box>
+    <Box minHeight='95vh'>
+      <Box>
+        <div style = {{
+          background: 'linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(121,190,210,1) 0%, rgba(178,5,220,1) 100%)',
+          zIndex:10,
+          height:'300px'
+        }}
+        />
+        <ChannelCard channelDetail={channelDetail} marginTop='-110px'/>
+      </Box>
+      <Box display='flex' p='2'>
+        <Box sx={{mr:{sm:'100px'}}}/>
+        <Videos videos={videos} />
+      </Box>
+    </Box>
   )
 }
 
